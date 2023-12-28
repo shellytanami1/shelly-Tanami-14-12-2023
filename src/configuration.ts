a01d25ac50656925f2cd3c69c28c8bf0b68773e6
shellyt <shellyt.okiano@gmail.com>
2023-12-28 21:01:16 +0200

    fix https

--- src/configuration.ts
+++ src/configuration.ts
@@ -1,7 +1,7 @@
 export const configuration = {
 
   apiKey: "i8IJYZrdDsvtCTpbHdBOZIEndujVYWm1",
-  apiHost: "http://dataservice.accuweather.com",
+  apiHost: "https://dataservice.accuweather.com",
 
   autoCompleteUrl:"locations/v1/cities/autocomplete",
   currentWeatherUrl:"currentconditions/v1",
